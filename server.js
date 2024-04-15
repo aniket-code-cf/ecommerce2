412ab7c9ae3312346db9208c9c7cfdadaa1f4f14
aniket-code-cf <aniketkumar74974@gmail.com>
2024-04-15 19:20:59 +0530

    new

--- server.js
+++ server.js
@@ -9,40 +9,40 @@ import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
 
-
-//configure env
+// Configure environment
 dotenv.config();
 
-//databse config
+// Database config
 connectDB();
 
-//rest object
+// Create Express app
 const app = express();
 
-//middelwares
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, './frontend/build')))
 
-//routes
+// Serve static files
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+app.use(express.static(path.join(__dirname, './frontend/build')));
+
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
-app.use('*',function(req,res)
-{
-  res.sendFile(path.join(__dirname,'./frontend/build/index.html'));
-})
+// REST API - Catch all other routes and serve the index.html file
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+});
 
-//PORT
+// Define PORT
 const PORT = process.env.PORT || 8080;
 
-//run listen
+// Start server
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-      .white
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white
   );
 });
